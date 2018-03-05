e48997d0eeee1e371946e58c94dd4590a8b2b21c
Rafael Roman <panthro.rafael@gmail.com>
2018-03-05 18:28:23 +0100

    list component first draft

--- lib/components/projetcs/material_list_component.dart
+++ lib/components/projetcs/material_list_component.dart
@@ -1,14 +1,35 @@
+import 'package:crochet_land/components/supplies/add_supply_form.dart';
+import 'package:crochet_land/components/supplies/supply_list.dart';
 import 'package:flutter/material.dart';
 
-class ProjectMaterials extends StatelessWidget {
+
+class ProjectMaterials extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _ProjectMaterialState();
+  }
+}
+
+class _ProjectMaterialState extends State<ProjectMaterials> {
+
+
   @override
   Widget build(BuildContext context) {
-    return new Container(padding: new EdgeInsets.all(20.0),
-        child: new Center(child: new Text(
-          "Aqui vai aparecer a lista de materiais usados nesse projeto, legal né?",
-          textAlign: TextAlign.center,
-        ),
-        )
+    return new Container(padding: new EdgeInsets.all(8.0),
+        child: new Column(children: <Widget>[
+          new SupplyList(),
+          new Expanded(child:
+          new Column(
+            children: <Widget>[
+              new Divider(), new AddSupplyForm(onAddSupply: (supply) {
+                // TODO actually saved something
+                debugPrint('$supply added');
+              },),
+            ],
+            mainAxisAlignment: MainAxisAlignment.end,
+          )),
+
+        ])
     );
   }
 }
