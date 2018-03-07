fc6eaac68a81fb828db3643aac7de30731ca5cfd
Rafael Roman <panthro.rafael@gmail.com>
2018-03-07 02:28:02 +0100

    testing new project form

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:crochet_land/services/auth.dart';
+import 'package:crochet_land/services/project_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mockito/mockito.dart';
 
@@ -9,4 +10,6 @@ class MockAuth extends Mock implements Auth {}
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
 class MockFirebaseUser extends Mock implements FirebaseUser {}
+
+class MockProjectService extends Mock implements ProjectService {}
 
