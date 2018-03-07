bad6a382ee4289c497c7da7cafe2d5055457c827
Rafael Roman <panthro.rafael@gmail.com>
2018-03-07 01:25:36 +0100

    doing some coverage with travis :-)

--- dev/travis_script.sh
+++ dev/travis_script.sh
@@ -5,7 +5,7 @@ export FLUTTER_HOME=$(pwd)/flutter
 
 export PATH="$FLUTTER_HOME/bin/cache/dart-sdk/bin:$PATH"
 
-flutter test
+flutter test --coverage
 
 # never managed to get the emulator up and running on travis
 # flutter drive --target=test_driver/login.dart
