b5814677bd22a781127b85e151c98e542d591a76
Rafael Roman <panthro.rafael@gmail.com>
2018-03-07 18:40:01 +0100

    more mocks

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,5 +1,7 @@
 import 'package:crochet_land/services/auth.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mockito/mockito.dart';
 
 
@@ -9,4 +11,10 @@ class MockAuth extends Mock implements Auth {}
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
 class MockFirebaseUser extends Mock implements FirebaseUser {}
+
+class MockFirebaseAnalytics extends Mock implements FirebaseAnalytics {}
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+
 
