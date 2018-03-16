a63cb1d74716ee349da6b1c75a9a4bcb551364c9
Rafael Roman <panthro.rafael@gmail.com>
2018-03-16 22:45:50 +0100

    disable patter visualization for now

--- lib/components/projetcs/project_details.dart
+++ lib/components/projetcs/project_details.dart
@@ -1,15 +1,10 @@
 import 'package:crochet_land/components/projetcs/counter_component.dart';
 import 'package:crochet_land/components/projetcs/project_supply_list_component.dart';
 import 'package:crochet_land/model/project.dart';
-import 'package:crochet_land/services/analytics.dart';
 import 'package:crochet_land/services/project_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:share/share.dart';
-
 
 class ProjectWidget extends StatefulWidget {
-
   Project _project;
 
   ProjectWidget(this._project);
@@ -18,16 +13,15 @@ class ProjectWidget extends StatefulWidget {
   State<StatefulWidget> createState() {
     return new _ProjectWidgetState(_project);
   }
-
-
 }
 
 class _ProjectWidgetState extends State<ProjectWidget> {
   Project _project;
-  String _webViewUrl;
+
+// TODO find a better way to show the pattern
+//  String _webViewUrl;
 
   _ProjectWidgetState(this._project);
-
 
   @override
   void dispose() {
@@ -36,47 +30,48 @@ class _ProjectWidgetState extends State<ProjectWidget> {
   }
 
   _buildActionbarActions(BuildContext context) {
-    if (_project.patternUrl == null) {
-      return null;
-    }
-    _webViewUrl = _project.patternUrl;
-    var flutterWebviewPlugin = new FlutterWebviewPlugin();
-    flutterWebviewPlugin.onUrlChanged
-        .listen((url) => this.setState(() => _project.patternUrl = url));
-
-    return <Widget>[
-      new IconButton(
-          icon: new Icon(Icons.open_in_new),
-          onPressed: () {
-            AnalyticsService.analytics.logViewItem(
-                itemId: _project.key + '-pattern',
-                itemName: _project.title,
-                itemCategory: 'project-patterns');
-            //TODO there's an error that the url is being saved if the user navigates away from the pattern url
-            Navigator.of(context).push(new MaterialPageRoute<Null>(
-              maintainState: false,
-              fullscreenDialog: true,
-              builder: (BuildContext context) {
-                return new WebviewScaffold(
-                  url: _project.patternUrl,
-                  appBar: new AppBar(
-                    title: new Text("Padrão"),
-                    actions: <Widget>[
-                      new IconButton(
-                          icon: new Icon(Icons.share),
-                          onPressed: () {
-                            AnalyticsService.analytics.logShare(
-                                contentType: 'pattern-url',
-                                itemId: _project.key);
-                            share(_webViewUrl);
-                          })
-                    ],
-                  ),
-                );
-              },
-            ));
-          })
-    ];
+    // TODO find a better way to show the pattern
+//    if (_project.patternUrl == null) {
+    return null;
+//    }
+//    _webViewUrl = _project.patternUrl;
+//    var flutterWebviewPlugin = new FlutterWebviewPlugin();
+//    flutterWebviewPlugin.onUrlChanged
+//        .listen((url) => this.setState(() => _project.patternUrl = url));
+//
+//    return <Widget>[
+//      new IconButton(
+//          icon: new Icon(Icons.open_in_new),
+//          onPressed: () {
+//            AnalyticsService.analytics.logViewItem(
+//                itemId: _project.key + '-pattern',
+//                itemName: _project.title,
+//                itemCategory: 'project-patterns');
+//            //TODO there's an error that the url is being saved if the user navigates away from the pattern url
+//            Navigator.of(context).push(new MaterialPageRoute<Null>(
+//              maintainState: false,
+//              fullscreenDialog: true,
+//              builder: (BuildContext context) {
+//                return new WebviewScaffold(
+//                  url: _project.patternUrl,
+//                  appBar: new AppBar(
+//                    title: new Text("Padrão"),
+//                    actions: <Widget>[
+//                      new IconButton(
+//                          icon: new Icon(Icons.share),
+//                          onPressed: () {
+//                            AnalyticsService.analytics.logShare(
+//                                contentType: 'pattern-url',
+//                                itemId: _project.key);
+//                            share(_webViewUrl);
+//                          })
+//                    ],
+//                  ),
+//                );
+//              },
+//            ));
+//          })
+//    ];
   }
 
   _buildSilverAppBar(BuildContext context, bool innerBoxIsScrolled) {
@@ -114,7 +109,10 @@ class _ProjectWidgetState extends State<ProjectWidget> {
   Widget build(BuildContext context) {
     if (_project == null) {
       return new Scaffold(
-        body: new Center(child: new CircularProgressIndicator(),),);
+        body: new Center(
+          child: new CircularProgressIndicator(),
+        ),
+      );
     }
     var tabView = _buildTabBarView();
     return new DefaultTabController(
