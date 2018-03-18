f094e3ec41431bca56ff6b5122f6e94be926b5bb
Rafael Roman <panthro.rafael@gmail.com>
2018-03-18 01:57:38 +0100

    adding animation and refresh post list

--- lib/components/blog_post_list.dart
+++ lib/components/blog_post_list.dart
@@ -8,38 +8,55 @@ class BlogPostList extends StatefulWidget {
 }
 
 class _BlogPostListState extends State<BlogPostList> {
+  final GlobalKey<AnimatedListState> _listKey = new GlobalKey<AnimatedListState>();
+  final GlobalKey<RefreshIndicatorState> _refreshKey = new GlobalKey<RefreshIndicatorState>();
+
   List<Map<String, dynamic>> _posts = [];
+
+  final durationBase = const Duration(milliseconds: 2000);
 
   _BlogPostListState();
 
-
   @override
   void initState() async {
-    var posts = await new WordpressPostService().loadPostsFromApi();
-    setState(() {
-      this._posts = posts.toList();
+    _refreshKey.currentState.show();
+  }
+
+  _loadBlogPosts() async {
+    Iterable<Map<String, dynamic>> posts = await new WordpressPostService().loadPostsFromApi();
+    _posts.clear();
+    posts.forEach((post) {
+      setState(() {
+        var index = this._posts.length;
+        this._posts.add(post);
+        this._listKey.currentState.insertItem(index,
+            duration: new Duration(seconds: durationBase.inSeconds, milliseconds: 500 * index));
+      });
     });
   }
 
-
   Widget _buildBody(BuildContext context) {
-    if (_posts.isEmpty) {
-      return new Center(child: new CircularProgressIndicator());
-    }
-    return new AnimatedList(itemBuilder: (context, index, animation) {
-      var post = _posts[index];
-      return new BlogPostCard(
-          post['link'], post['featured_media'], post['title']['rendered'],
-          post['summary']['rendered']);
-    }, initialItemCount: _posts.length);
+    return new RefreshIndicator(
+        key: _refreshKey,
+        onRefresh: _loadBlogPosts,
+        child: new AnimatedList(
+            key: _listKey,
+            itemBuilder: (context, index, animation) {
+              var post = _posts[index];
+              return new FadeTransition(
+                  opacity: animation,
+                  child: new BlogPostCard(
+                    post['link'],
+                    post['featured_media'],
+                    post['title']['rendered'],
+                    post['summary']['rendered'],
+                  ));
+            },
+            initialItemCount: _posts.length));
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(body: _buildBody(context),
-      appBar: new AppBar(title: new Text("Novidades"),),);
+    return _buildBody(context);
   }
 }
-
-
