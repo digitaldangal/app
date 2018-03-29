544b8125de1c482c671fcf4b687be83257d1bb58
Rafael Roman <panthro.rafael@gmail.com>
2018-03-29 19:58:11 +0200

    login test fixed

--- test/login_test.dart
+++ test/login_test.dart
@@ -2,60 +2,60 @@ import 'dart:async';
 
 import 'package:crochet_land/components/login/login.dart';
 import 'package:crochet_land/config/routes.dart';
+import 'package:crochet_land/services/auth.dart';
+import 'package:crochet_land/stores/user_store.dart';
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:service_registry/service_registry.dart';
 
 import 'mocks.dart';
 
 void main() {
   MockAuth authMock;
 
-  MockFirebaseAuth mockFirebaseAuth;
+  UserStore userStore;
 
-  StreamController<MockFirebaseUser> userChangeStreamController;
-
-  loggedOut() {
-    userChangeStreamController.add(null);
-  }
-
-
-  loggedIn() {
-    var user = new MockFirebaseUser();
-    when(user.email).thenReturn('someone@gmail.com');
-    userChangeStreamController.add(user);
-  }
+  Router router;
 
   setUp(() {
+    userStore = new UserStore();
     authMock = new MockAuth();
-    mockFirebaseAuth = new MockFirebaseAuth();
-    when(authMock.firebaseAuth).thenReturn(mockFirebaseAuth);
-    userChangeStreamController = new StreamController<MockFirebaseUser>();
+    router = new Router();
 
-    when(mockFirebaseAuth.onAuthStateChanged).thenReturn(
-        userChangeStreamController.stream);
-
-    Login.auth = authMock;
+    ServiceRegistry.registerService(Router, router);
+    ServiceRegistry.registerService(UserStore, userStore);
+//    ServiceRegistry.registerService(FirebaseAuth, mockFirebaseAuth);
+    ServiceRegistry.registerService(AuthenticationService, authMock);
   });
 
-  tearDown(() {
-    userChangeStreamController.close();
-  });
+  loggedOut() async {
+    await UserStore.updateUserAction(null);
+  }
+
+  loggedIn() async {
+    var user = new MockFirebaseUser();
+    when(user.email).thenReturn('someone@gmail.com');
+    await UserStore.updateUserAction(user);
+  }
 
   testWidgets('Login with Google', (WidgetTester tester) async {
-    loggedOut();
+    await loggedOut();
 
     bool routeHomeCalled = false;
 
-    await tester.pumpWidget(new MaterialApp(home: new Login(),
-        routes: {Routes.home: (_) {
+    router.define(Routes.home,
+        handler: new Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
           routeHomeCalled = true;
-          return new Text('route_home');
-        }}));
+          return new Text("Home");
+        }));
+
+    await tester.pumpWidget(
+      new MaterialApp(home: new Login(), onGenerateRoute: (_) => router.generator(_)),
+    );
 
     expect(find.text("Carregando..."), findsOneWidget);
-
-//    await untilCalled(mockFirebaseAuth.onAuthStateChanged);
 
     await tester.pump();
 
@@ -63,13 +63,11 @@ void main() {
 
     expect(loginGoogleFinder, findsOneWidget);
 
-    when(authMock.signInWithGoogle()).thenAnswer((_) =>
-    new Future.value('Happy user'));
+    when(authMock.signInWithGoogle()).thenAnswer((_) => new Future.value('Happy user'));
 
     loggedIn();
 
-    await tester.tap(find.ancestor(
-        of: loginGoogleFinder, matching: find.byType(RaisedButton)));
+    await tester.tap(find.ancestor(of: loginGoogleFinder, matching: find.byType(RaisedButton)));
 
     await tester.pump(new Duration(milliseconds: 100));
 
